801827af3d592e9909937c7b47152bf8a8005842
igoreineck <hauzip@gmail.com>
2015-09-02 15:55:30 -0300

    updated

---
+++ 2015-s2/igoreineck/Cap3/ex_3.13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void){
+
+	int x = 1, total = 0, y;
+
+	while (x <= 10){
+		y = x * x;
+		printf("%d\n", y);
+		total += y;
+		++x;
+	}
+
+	printf("Total is %d\n", total);
+	return 0;
+}
