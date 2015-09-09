aef57626d759e5f0d1641cb37d658534710d698c
gianricardo <gianricardo@users.noreply.github.com>
2015-09-09 13:21:00 -0300

    map de bits

---
+++ codes/map-bit/exemplo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+typedef unsigned long Reg32;
+
+typedef union {
+	struct {
+	    Reg32 version_id:8;
+	    Reg32 num_counters:8;
+	    Reg32 bit_width:8;
+	    Reg32 mask_length:8;
+	} split;
+	Reg32 full;
+    } cpu_reg;
+    
+void displayBits( unsigned value )
+{ 
+   unsigned c; 
+   unsigned displayMask = 1 << 31;
+   printf( "%10u = ", value );
+   for ( c = 1; c <= 32; c++ ) { 
+      putchar( value & displayMask ? '1' : '0' );
+      value <<= 1; 
+      if ( c % 8 == 0 ) { 
+         putchar( ' ' );
+      }
+   } 
+   putchar( '\n' );
+} 
+    
+int main( void )
+{ 
+   srand( time( NULL ) );
+   int num =1;
+   
+   cpu_reg registro;
+   
+   while(num != 0){
+     registro.full = ( rand() % UINT_MAX );
+     printf("\t\tNumero: %32u\n", registro.full );
+     displayBits( registro.full );
+     displayBits( registro.split.version_id );
+     displayBits( registro.split.num_counters );
+     displayBits( registro.split.bit_width );
+     displayBits( registro.split.mask_length );
+     printf("Deve continuar a gerar números? (0 para encerrar):  ");
+     scanf("%d",&num);
+   }
+   
+   return 0;
+}
+   
